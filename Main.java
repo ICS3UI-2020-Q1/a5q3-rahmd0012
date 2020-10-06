955ce4b4c393c4e2a7098b36fc9e23b82c979b45
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-06 13:56:46 +0000

    Finding the factorial of a user entered number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program to find the factorial of a user entered number
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for an integer
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int n = input.nextInt();
+
+    // the accumulator variable to add the count into
+    int total = 1;
+
+    // create a for loop
+    for(int count = n; count >= 1; count--){
+      // create a total for the numbers decsending
+      total = total * count;
+    }
+    // print the total to the screen
+    System.out.println(n + "! = " + total);
   }
 }
